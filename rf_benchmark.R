c5446e239a8286d11cf7fa8171d4405a0a0d1bba
LinguisticsStudent <student.linguistics@gmail.com>
2014-02-10 10:40:34 -0500

    Create rf_benchmark.R
    A random forest solution from kaggle.com tutorial

---
+++ rf_benchmark.R
@@ -0,0 +1,15 @@
+# makes the random forest submission
+# from kaggle.com tutorial
+
+library(randomForest)
+
+train <- read.csv("../data/train.csv", header=TRUE)
+test <- read.csv("../data/test.csv", header=TRUE)
+
+labels <- as.factor(train[,1])
+train <- train[,-1]
+
+rf <- randomForest(train, labels, xtest=test, ntree=1000)
+predictions <- levels(labels)[rf$test$predicted]
+
+write(predictions, file="rf_benchmark.csv", ncolumns=1) 
