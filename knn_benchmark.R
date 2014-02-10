38c6a3579c64bb7a3f2d853a5592a25f073d4bd8
LinguisticsStudent <student.linguistics@gmail.com>
2014-02-10 16:28:25 -0500

    Update knn_benchmark.R

--- knn_benchmark.R
+++ knn_benchmark.R
@@ -12,6 +12,8 @@ train <- train[,-1]
 results <- (0:9)[knn(train, test, labels, k = 10, algorithm="cover_tree")]
 
 write(results, file="knn_benchmark.csv", ncolumns=1) 
-# This is not the submission file. It includes the prediction, but not the ImageId. One can either input the document into a spss file and generate the ID through SPSS, or generate the ID by using R. Either one should work.
 
-# The matching rate is around 96.557%.
+# This is not the submission file. It includes the prediction, but not the ImageId. 
+# One can either input the document into a spss file and generate the ID through SPSS, 
+# or generate the ID by using R. Either one should work.
+# The matching rate is 96.557%.
